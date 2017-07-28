5c1ac8246ac026dc2914f6bea05e43d9bc6db100
Avijit Pal <avijitpal.1987@gmail.com>
2017-07-28 12:34:03 +0530

    commit

--- lib/facter/myos.rb
+++ lib/facter/myos.rb
@@ -1,7 +1,7 @@
 Facter.add('myos') do
   setcode do
     begin
-    Facter::Core::Execution.exec('sleep 10', :timeout => 5)
+    Facter::Core::Execution.execute('sleep 10', :timeout => 5)
      'Did not timeout'
    rescue Facter::Core::Execution::ExecutionFailure
       'timeout'
